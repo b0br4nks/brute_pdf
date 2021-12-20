63954ac16e259ce94b55d4c78d1668960852f6bd
Noam Zeitoun Rocamora <77684294+GeneralNZR@users.noreply.github.com>
2021-12-20 11:01:53 +0100

    add color variables

--- brute.py
+++ brute.py
@@ -2,7 +2,12 @@ import pikepdf
 
 from tqdm import tqdm
 
-print("""\033[1;35;40m
+mag = "\033[0;35;40m"
+cya = "\033[0;36;40m"
+yel = "\033[0;33;40m"
+whi = "\033[1;37;40m"
+
+print(f"""{mag}
 ██████  ██████  ██    ██ ████████ ███████         ██████  ██████  ███████ 
 ██   ██ ██   ██ ██    ██    ██    ██              ██   ██ ██   ██ ██      
 ██████  ██████  ██    ██    ██    █████           ██████  ██   ██ █████   
@@ -12,19 +17,19 @@ print("""\033[1;35;40m
 
 key = [x.strip() for x in open("keylist.txt")]
 
-name_file = input("\033[1;36;40m[?]Enter the name of the file: ")
+name_file = input(f"{cya}[?]Enter the name of the file: ")
 
-for z in tqdm(key, "\033[1;33;40m[?]Brute-force in progress..."):
+for z in tqdm(key, f"{yel}[?]Brute-force in progress..."):
     try:
         with pikepdf.open(name_file+".pdf", password=z) as pdf:
-            print("\n\n\n\033[1;35;40m[!]Cracked key:", z+ "\n\n")
-            close = input("\033[1;36;40m[?]Save the cracked file? (y/n): ")
+            print(f"\n\n\n{mag}[!]Cracked key:", f"{whi}{z}\n\n")
+            close = input(f"{cya}[?]Save the cracked file? (y/n): ")
             if close == "y":
                 pdf.save(name_file+"-cracked.pdf")
-                print("\033[1;35;40m[!]File saved as: "+name_file+"-cracked.pdf\n\n")
+                print(f"{mag}[!]File saved as: "+name_file+"-cracked.pdf\n\n")
                 break
             else:
-                print("\033[1;35;40m[!]File not saved\n\n")
+                print(f"{mag}[!]File not saved\n\n")
                 exit()
     except pikepdf._qpdf.PasswordError as e:
         continue
